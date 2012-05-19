d72ba7c900823975df31c465d587f066f9ce302c
Mike Bannister <notimpossiblemike@gmail.com>
2012-05-19 09:57:53 +0200

    show sources in prefs pane

--- client/client.js
+++ client/client.js
@@ -1,11 +1,21 @@
+StorySources = new Meteor.Collection('storySources');
 Stories = new Meteor.Collection('stories');
 Blog = new Meteor.Collection('blogs');
 
+Meteor.subscribe('storySources');
 Meteor.subscribe('stories');
 Meteor.subscribe('blogs');
 
 Template.blog.blog = function() {
   return Blog.findOne();
+};
+
+Template.preferencesPane.blog = function() {
+  return Blog.findOne();
+};
+
+Template.preferencesPane.sources = function() {
+  return StorySources.find();
 };
 
 Template.blog.storyList = function() {
@@ -15,3 +25,8 @@ Template.blog.storyList = function() {
 Template.story.publishedAt = function() {
   return moment(new Date(this.publishedAt)).format('MMMM do YYYY');
 };
+
+Template.preferencesActivator.currentUser = UserSessionHelpers.currentUser;
+// TODO: these should be moved out of user-sessions if they're generically userful
+Template.preferencesActivator.events = UserSessionHelpers.commonActivatorEvents;
+Template.preferencesPane.events = UserSessionHelpers.commonFormEvents;
